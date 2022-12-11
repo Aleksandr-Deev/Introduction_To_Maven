33a52e5784cc5721d415f7dd6d0f2c6adbfff511
Aleksandr Deev <geshtait@mail.ru>
2022-12-11 18:42:12 +0500

    Первый коммит

---
+++ src/main/java/com/example/introduction_to_maven/IntroductionToMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.introduction_to_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionToMavenApplication.class, args);
+    }
+
+}
